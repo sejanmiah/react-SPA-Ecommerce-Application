ec8211af6e9c95ae3dfb4f1c1032fb1fecd50c3f
sejanmiah <sejankhan931@gmail.com>
2025-04-20 03:10:32 +0400

    Product single page layout css thik korlam, onk rat hoise tai gumate jai, allah bacaile inshallah kalke abar suro korbo, allah hafez

--- src/component/PhoneDetails/PhonesDetails.jsx
+++ src/component/PhoneDetails/PhonesDetails.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const PhonesDetails = () => {
     return (
-        <div>
-            <h1>This is phone detai;ls page</h1>
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+            <div></div>
+            <div></div>
         </div>
     );
 };
